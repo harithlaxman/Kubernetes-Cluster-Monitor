8a7b9f1e789d71766d664338a7f43105bb9afa06
Harith Laxman <harithlaxman@gmail.com>
2021-07-22 09:04:14 +0530

    Final

--- alpha-pod/controller/controller.py
+++ alpha-pod/controller/controller.py
@@ -3,8 +3,8 @@ import yaml
 from kafka import KafkaConsumer
 import os 
 from prometheus_api_client import PrometheusConnect
-from math import ceil
 import sys
+import time
 
 KAFKA_IP = sys.argv[1]
 PROMETHEUS_SERVER = sys.argv[2]
@@ -13,8 +13,7 @@ RATIO = float(sys.argv[4])
 
 prometheus = PrometheusConnect(url=PROMETHEUS_SERVER, disable_ssl=True)
 
-consumer = KafkaConsumer(bootstrap_servers=KAFKA_IP,
-						 auto_offset_reset='earliest')
+consumer = KafkaConsumer(bootstrap_servers=KAFKA_IP)
 
 query_res = prometheus.custom_query(query=QUERY)
 
@@ -26,6 +25,9 @@ consumer.subscribe(['predictions'])
 for msg in consumer:
 	data = yaml.safe_load(msg.value.decode('utf-8'))
 	max_utilisation = max(data['predictions'])
-	scale_size = ceil(max_utilisation/memory_limit)
+	scale_size = round(max_utilisation/memory_limit)
 	if(scale_size>1):
 		os.system(f'kubectl scale --replicas={scale_size} -n monitoring deployments/prometheus')
+		time.sleep(178*60)
+		os.system(f'kubectl scale --replicas=1 -n monitoring deployments/prometheus')
+	
